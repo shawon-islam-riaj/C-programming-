71773cf3f08a2ee330ddd44573a21db59d9aad20
shawon-islam-riaj <shawoni047@gmail.com>
2024-12-14 22:01:02 +0600

    Add files via upload

---
+++ assignment 6 area of the circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float r, area;
+    printf("Enter Radius = ");
+    scanf("%f",&r);
+    area = 3.1426*r*r;
+    printf("The area of the circle is = %f\n",area);
+    return 0;
+
+}
